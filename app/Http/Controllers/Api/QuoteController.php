b84a4d72f13df51e7083554265bb302bbc88a7a4
Christian Partogi <cpartogi@gmail.com>
2024-04-05 21:04:58 +0700

    feat: quote

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class QuoteController extends Controller
+{
+
+    public function get_data ($url) {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+
+    public function index()
+    {
+        // chuck norris quote
+        $url = env('CHUCK_NORRIS_API_URL');
+
+        $chuck = $this->get_data($url);
+
+        $chuckData = json_decode($chuck, true);
+
+        $chuckQuote = $chuckData["value"];
+
+        $data["quote"] = $chuckQuote;
+        $data["quoteSource"] = "Chuck Norris"; 
+
+
+        //cat fact quote
+        $url = env('CAT_FACT_API_URL');
+
+        $cat = $this->get_data($url);
+
+        $catData = json_decode($cat, true);
+
+        $catFact = $catData["fact"];
+
+        $data["fact"] = $catFact;
+        $data["factSource"] = "Cat Facts"; 
+
+        // dog fact api currently offline
+
+        return response()->json([
+            "status" => "Success",
+            "statusCode" => 200,
+            "message" => "Success get quotes",
+            "data" => $data,
+        ], 200);
+
+
+    }
+}
